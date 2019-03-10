3b48bdfa6ead227f0ce04f1ffee433f1b3ea62ca
Rafael Melo <rsmelo_@hotmail.com>
2019-03-10 18:04:35 -0300

    Learned flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,160 +3,79 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
+Future fetchItem() async {
+    final response = await http.get('https://www.boredapi.com/api/activity?type=recreational');
+    if (response.statusCode == 200) {
+      return json.decode(response.body.toString());
+    } else {
+      throw Exception('Failed to load post');
+    }
+  }
+
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future items = fetchItem();
+
+  Future _fetchState() {
+    setState(() {
+      items = fetchItem();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Ricky and Morty Characters',
+      title: 'Boredom Destroyer',
       theme: new ThemeData(
         primarySwatch: Colors.indigo,
       ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Ricky and Morty Characters'),
+          title: new Text('Boredom Destroyer'),
         ),
-        body:new FutureBuilder(
-          future: fetchChars(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: 12,
-                itemBuilder: (context, index) {
-                  // // print('Howdy, ${snapshot.data.name}!');
-                  // print("===> ${snapshot.data}");
-                  return CardItem(snapshot.data);
-                },
-              );
-            } else if (snapshot.hasError) {
-              // print(snapshot.error);
-              return new Text("error: ${snapshot.error}");
-            }
+        body: new Container(
+          child: new FutureBuilder(
+            future: items,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return new Column(
+                  children: [new Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: new Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        new Text('What should I do?', style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+                        new ListTile(
+                          title: Text('${snapshot.data['activity']}', textAlign: TextAlign.center),
+                        ),
+                        new RaisedButton(
+                          onPressed: () { _fetchState(); },
+                          child: const Text('Hit me up!'),
+                        )
+                      ],
+                    ),
+                  ))],
+                );
+              } else if (snapshot.hasError) {
+                return new Text("${snapshot.error}");
+              }
 
-            // By default, show a loading spinner
-            return new Center(child: new CircularProgressIndicator());
-          },
-        )
-        // new Container(
-        //   child: new FutureBuilder<List>(
-        //     future: fetchChars(),
-        //     builder: (context, snapshot) {
-        //       if (snapshot.hasData) {
-        //         return ListView.builder(
-        //           itemCount: items.length,
-        //           itemBuilder: (context, index) {
-        //             return ListTile(
-        //               title: Text('${items[index]}'),
-        //             );
-        //           },
-        //         );
-        //       } else if (snapshot.hasError) {
-        //         return new Text("${snapshot.error}");
-        //       }
-
-        //       // By default, show a loading spinner
-        //       return new Center(child: new CircularProgressIndicator());
-        //     },
-        //   ),
-        // ),
-      ),
-    );
-  }
-
-
-
-  Future fetchChars() async {
-    final response = await http.get('https://rickandmortyapi.com/api/character');
-
-    // if (response.statusCode == 200) {
-    //   // If server returns an OK response, parse the JSON
-    //   return Char.fromJson(json.decode(response.body));
-    // } else {
-    //   // If that response was not OK, throw an error.
-    //   throw Exception('Failed to load post');
-    // }
-    Map<dynamic, dynamic> responseJson = json.decode(response.body.toString());
-    var chars = responseJson['results'];
-    // print('==>');
-    // print(chars);
-    for (var items in chars){ //iterate over the list
-      Map myMap = items; //store each map
-      // print(myMap['name']);
-    }
-    var teste = new List<dynamic>.generate(
-      chars.length,
-      (index) => chars,
-    );
-    print('LISTAAAAAAAAAAAAAAA');
-    print(teste);
-    print(teste[0]);
-    return chars;
-  }
-
-}
-
-
-class CharTile extends StatelessWidget {
-  final _chat;
-  CharTile(this._chat);
-
-  @override
-  Widget build(BuildContext context) => new Column(
-    children: <Widget>[
-      new ListTile(
-        title: Text(_chat.name),
-        subtitle: Text(_chat.tagline),
-      ),
-      Divider()
-    ],
-  );
-}
-
-class CardItem extends StatelessWidget {
-  final _char;
-  CardItem(this._char);
-
-  @override
-  Widget build(BuildContext context) {
-    // print('carditem');
-    // print(_char);
-    return  Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const ListTile(
-            leading: Icon(Icons.album),
-            title: Text('The Enchanted Nightingale'),
-            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+              // By default, show a loading spinner
+              return new Center(child: new CircularProgressIndicator());
+            },
           ),
-        ],
+        ),
       ),
     );
   }
 }
-
-abstract class ListItem {}
-
-// class Char {
-//   int id;
-//   String name;
-//   String status;
-//   String species;
-//   String type;
-//   String gender;
-
-//   Char({this.id, this.name, this.status, this.species, this.type, this.gender});
-
-//   factory Char.fromJson(Map<String, dynamic> json) {
-//     return Char(
-//       id: json['id'],
-//       name: json['name'],
-//       status: json['status'],
-//       species: json['species'],
-//       type: json['type'],
-//       gender: json['gender'],
-//     );
-//   }
-// }
